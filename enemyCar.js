6bb105ffe48bbf0195e36233facbeae8f02ce440
Beauti <183709979+beautisui@users.noreply.github.com>
2025-03-17 22:52:47 +0530

    avoided getBoundingClientRect() function and use class property to get the details of the car position

--- enemyCar.js
+++ enemyCar.js
@@ -7,28 +7,28 @@ class EnemyCar {
       this.carEmojis[Math.floor(Math.random() * this.carEmojis.length)];
 
     this.roadWidth = roadWidth;
-    this.positionX = Math.floor(Math.random() * (roadWidth - 50));
+    this.carWidth = 40;
+    this.carHeight = 40;
+    this.positionX = Math.floor(Math.random() * (roadWidth - this.carWidth));
     this.positionY = -100;
-    this.moveInterval = null;
 
+    this.element.style.width = this.carWidth + "px";
+    this.element.style.height = this.carHeight + "px";
     this.element.style.left = this.positionX + "px";
+
     road.appendChild(this.element);
   }
 
   checkCollision(playerCar) {
-    const playerRect = playerCar.element.getBoundingClientRect();
-    const enemyRect = this.element.getBoundingClientRect();
-
     return (
-      playerRect.left < enemyRect.right &&
-      playerRect.right > enemyRect.left &&
-      playerRect.top < enemyRect.bottom &&
-      playerRect.bottom > enemyRect.top
+      this.positionX < playerCar.positionX + playerCar.carWidth &&
+      this.positionX + this.carWidth > playerCar.positionX &&
+      this.positionY < playerCar.positionY + playerCar.carHeight &&
+      this.positionY + this.carHeight > playerCar.positionY
     );
   }
 
   destroy() {
-    clearInterval(this.moveInterval);
     this.element.remove();
   }
 }
